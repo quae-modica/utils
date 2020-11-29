72c30a26976c307a750c7618c2ecb22bc04eb982
Sam Lanzo <quae.modica@gmail.com>
2020-11-29 19:54:14 +0000

    fork for client and server tests

--- main.cc
+++ main.cc
@@ -1,16 +1,29 @@
 #include "SocketInterface.h"
+#include "Log.h"
 using namespace QuaeModicaUtil;
+
+void serverTest() {
+  SocketInterface server("localhost", "24601", true);
+  std::string response = server.receive();
+  printf("server received: %s", response.c_str());
+}
+
+void clientTest() {
+  SocketInterface client("localhost", "24601");
+  std::string message = "I am the very model of a modern major general";
+  printf("client sending:  %s", message.c_str());
+  client.send(message);
+}
 
 int main(int argc, char **argv) {
   (void) argc;
   (void) argv;
-  SocketInterface server("localhost", "24601", true);
-  SocketInterface client("localhost", "24601");
-
-  std::string message = "I am the very model of a modern major general";
-  client.send(message);
-  std::string response = server.receive();
-  std::cout << message << std::endl;
-  std::cout << response << std::endl;
+  SET_LOG(DEBUG, true);
+  if (fork() == 0) {
+    serverTest();
+  }
+  if (fork() == 0) {
+    clientTest();
+  }
   return 0;
 }
